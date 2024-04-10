3f56e06eea6a9c0bbea285bcd87701cece4c2935
luisca12 <solera.luis12@gmail.com>
2024-04-10 11:51:16 -0600

    commit number 4

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from commandsCLI import *
 from log import *
 import os
 
-greetingString()
-validIPs, username, netDevice = Auth()
+def main():
+    greetingString()
+    validIPs, username, netDevice = Auth()
 
-while True:
-    print("* Only numbers are accepted *")
-    menuString(validIPs, username), print("\n")
-    selection = input("Please choose the option that you want: ")
-    if checkIsDigit(selection):
-        if selection == "1":
-            # This option will show the interfaces not connected.
-            shCommands(validIPs, username, netDevice)
-        if selection == "2":
-            authLog.info(f"User {username} logged out from the program.")
-            break
-    else:
-        authLog.error(f"Wrong option chosen {selection}")
-        inputErrorString()
-        os.system("PAUSE")
+    while True:
+        print("* Only numbers are accepted *")
+        menuString(validIPs, username), print("\n")
+        selection = input("Please choose the option that you want: ")
+        if checkIsDigit(selection):
+            if selection == "1":
+                # This option will show the interfaces not connected.
+                shCommands(validIPs, username, netDevice)
+            if selection == "2":
+                authLog.info(f"User {username} logged out from the program.")
+                break
+        else:
+            authLog.error(f"Wrong option chosen {selection}")
+            inputErrorString()
+            os.system("PAUSE")
+
+if __name__ == "__main__":
+    main()
